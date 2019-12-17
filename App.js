ba2fc0fa7bb6f2f288ecf0b4a3da0617a65a691a
StuBeezy <45637277+StuBeezy@users.noreply.github.com>
2019-12-17 18:01:45 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Photos from './components/Photos';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Photos/>
+    </div>
+  );
+}
+
+export default App;
